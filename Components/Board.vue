4d55649e05a662683f3db37df7b511daea3dd45b
yendefrr <github@yendefr.xyz>
2024-06-15 20:45:24 +0300

    fix(emits): added actions emits to board

--- Components/Board.vue
+++ Components/Board.vue
@@ -8,6 +8,7 @@ import Dialog from "primevue/dialog";
 import KanbanColumn from "./Column.vue";
 
 const props = defineProps(["modelValue", "cards", "columns"]);
+const emit = defineEmits(["addCard", "moveCard", "editStatus"]);
 
 const cards = ref(props.cards);
 const columnStatuses = ref(props.columns);
@@ -32,6 +33,7 @@ const addCard = (card, status) => {
   };
 
   cards.value.push(finalCard);
+  emit("addCard", finalCard);
 };
 
 const moveCard = (cardId, newStatus) => {
@@ -39,6 +41,7 @@ const moveCard = (cardId, newStatus) => {
   if (card) {
     card.status = newStatus;
   }
+  emit("moveCard", card);
 };
 const drop = (event, receivedStatus) => {
   const column = event.dataTransfer.getData("text/plain");
@@ -52,21 +55,26 @@ const moveElement = (array, fromIndex, toIndex) => {
   array.splice(toIndex, 0, element);
 
   return array;
-}
+};
 
 const editStatus = (newStatus, oldStatus) => {
   columnStatuses.value[columnStatuses.value.indexOf(oldStatus)] = newStatus;
+  emit("editStatus", newStatus, oldStatus);
 };
-
 </script>
 
 <template>
   <div class="flex w-full justify-start min-h-screen overflow-scroll">
-    <div v-for="status in columnStatuses" class="flex" @dragover.prevent
-      @drop="props.modelValue ? drop($event, status) : null">
-      <KanbanColumn :modelValue="props.modelValue" :key="status" :status="status"
-        :cards="cards.filter((card) => card.status === status)" @addCard="addCard" @moveCard="moveCard"
-        @editStatus="editStatus"></KanbanColumn>
+    <div v-for="status in columnStatuses" class="flex" @dragover.prevent @drop="props.modelValue ? drop($event, status) : null">
+      <KanbanColumn
+        :modelValue="props.modelValue"
+        :key="status"
+        :status="status"
+        :cards="cards.filter((card) => card.status === status)"
+        @addCard="addCard"
+        @moveCard="moveCard"
+        @editStatus="editStatus"
+      ></KanbanColumn>
     </div>
     <Button class="h-[40px] min-w-[40px]" icon="pi pi-plus" @click="visibleDialog = true" outlined />
   </div>
@@ -77,8 +85,7 @@ const editStatus = (newStatus, oldStatus) => {
       <InputText id="username" class="flex-auto" v-model="dialogData.title" />
     </div>
     <div class="flex">
-      <Button class="w-full" type="button" label="Save" @click="addColumn"
-        v-bind:disabled="dialogData.title.length < 1"></Button>
+      <Button class="w-full" type="button" label="Save" @click="addColumn" v-bind:disabled="dialogData.title.length < 1"></Button>
     </div>
   </Dialog>
 </template>
